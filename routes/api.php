43bfd2c293caa3410788401f80a4e19f3cd79e8e
BorisGautier <tchoukouahaboris@outlook.fr>
2022-04-04 10:04:54 +0100

    add authentification

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('auth/email/verify/{id}', [App\Http\Controllers\Api\VerificationController::class, 'verify'])->name('verification.verify');
+Route::get('auth/email/resend', [App\Http\Controllers\Api\VerificationController::class, 'resend'])->name('verification.resend');
+
+Route::middleware('auth.apikey')->group(
+    function () {
+
+
+        Route::post('auth/password/forgot', [App\Http\Controllers\Api\UserController::class, 'forgot']);
+        Route::post('auth/password/reset', [App\Http\Controllers\Api\UserController::class, 'reset'])->name('password.reset');
+
+        Route::post('auth/register', [App\Http\Controllers\Api\UserController::class, 'register']);
+        Route::post('auth/login', [App\Http\Controllers\Api\UserController::class, 'login']);
+
+        Route::middleware('auth:api')->group(
+            function () {
+                Route::get('auth/logout', [App\Http\Controllers\Api\UserController::class, 'logout']);
+                Route::post('user/update/{id}', [App\Http\Controllers\Api\UserController::class, 'updateuser']);
+                Route::delete('user/delete/{id}', [App\Http\Controllers\Api\UserController::class, 'deleteuser']);
+                Route::get('user/me', [App\Http\Controllers\Api\UserController::class, 'getUser']);
+            }
+        );
+    }
+);
